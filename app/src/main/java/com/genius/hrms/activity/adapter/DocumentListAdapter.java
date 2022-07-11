44f23e44fd69aed539d60ef044563af77e834af2
GENIUS\sysdev_sidharthab <sidharthabehera405@gmail.com>
2022-07-11 14:56:23 +0530

    COMMIT BY SIDHARTHA

--- app/src/main/java/com/genius/hrms/activity/adapter/DocumentListAdapter.java
+++ app/src/main/java/com/genius/hrms/activity/adapter/DocumentListAdapter.java
@@ -3,6 +3,8 @@ package com.genius.hrms.activity.adapter;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,12 +23,17 @@ import com.genius.hrms.activity.profile.DocumentActivity;
 import com.genius.hrms.activity.profile.DocumentDetails;
 import com.genius.hrms.activity.utility.Pref;
 import com.genius.hrms.activity.utility.RecyclerItemClickListener;
+import com.google.cloud.translate.Translate;
+import com.google.cloud.translate.TranslateOptions;
+import com.google.cloud.translate.Translation;
 
 import java.util.ArrayList;
 
 public class DocumentListAdapter extends RecyclerView.Adapter<DocumentListAdapter.MyViewHolder> {
     ArrayList<DocumentListModel> reportList = new ArrayList<>();
+    Pref pref;
     Context mContex;
+    ProgressDialog pd;
 
 
 
@@ -40,10 +47,55 @@ public class DocumentListAdapter extends RecyclerView.Adapter<DocumentListAdapte
 
     @Override
     public void onBindViewHolder(@NonNull final MyViewHolder myViewHolder, final int pos) {
+        pref=new Pref(mContex);
+        pd=new ProgressDialog(mContex);
+        pd.setMessage("Loading..");
+        pd.setCancelable(true);
+        if (pref.getLanguage().equals("hi")) {
+            final Handler textViewHandler1 = new Handler();
+            new AsyncTask<Void, Void, Void>() {
+                @Override
+                protected Void doInBackground(Void... params) {
+                    TranslateOptions options = TranslateOptions.newBuilder()
+                            .setApiKey("AIzaSyCEQyxLkrIoD2-k_185t2EUKEc8IlggaMs")
+                            .build();
+                    Translate translate = options.getService();
+                    final Translation translation =
+                            translate.translate(reportList.get(pos).getHrDescription(),
+                                    Translate.TranslateOption.sourceLanguage("en"),  Translate.TranslateOption.targetLanguage(pref.getLanguage()));
+                    textViewHandler1.post(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            Log.d("sssh", translation.getTranslatedText());
+                            String hLoc = translation.getTranslatedText();
+                            myViewHolder.tvDocumentList.setText(hLoc);
 
 
-        myViewHolder.tvDocumentList.setText(reportList.get(pos).getHrDescription());
+                        }
+                    });
+                    return null;
+                }
 
+                @Override
+                protected void onPreExecute() {
+                    super.onPreExecute();
+                    pd.show();
+
+                }
+
+                @Override
+                protected void onPostExecute(Void aVoid) {
+                    super.onPostExecute(aVoid);
+                    pd.dismiss();
+
+                }
+
+
+            }.execute();
+        }else {
+            myViewHolder.tvDocumentList.setText(reportList.get(pos).getHrDescription());
+        }
         myViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -56,6 +108,8 @@ public class DocumentListAdapter extends RecyclerView.Adapter<DocumentListAdapte
 
             }
         });
+
+
 
 
 
--- app/src/main/java/com/genius/hrms/activity/model/DocumentDetailsAdapter.java
+++ app/src/main/java/com/genius/hrms/activity/model/DocumentDetailsAdapter.java
@@ -1,8 +1,12 @@
 package com.genius.hrms.activity.model;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,13 +16,19 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.genius.hrms.R;
+import com.genius.hrms.activity.utility.Pref;
+import com.google.cloud.translate.Translate;
+import com.google.cloud.translate.TranslateOptions;
+import com.google.cloud.translate.Translation;
 
 import java.util.ArrayList;
 
 public class DocumentDetailsAdapter extends RecyclerView.Adapter<DocumentDetailsAdapter.MyViewHolder> {
 
     ArrayList<DocumentDetailsModel> reportList = new ArrayList<>();
+    Pref pref;
     Context mContex;
+
 
     @NonNull
     @Override
@@ -28,10 +38,162 @@ public class DocumentDetailsAdapter extends RecyclerView.Adapter<DocumentDetails
     }
 
     @Override
-    public void onBindViewHolder(@NonNull DocumentDetailsAdapter.MyViewHolder holder, final int position) {
-        holder.tvLeaveAttendance.setText(reportList.get(position).getManualCategory());
-        holder.tvLeaveApplication.setText("( " + reportList.get(position).getManualSubCategory() + " )");
-        holder.tvDescriprtion.setText(reportList.get(position).getManualDescription());
+    public void onBindViewHolder(@NonNull final DocumentDetailsAdapter.MyViewHolder holder, final int position) {
+        pref=new Pref(mContex);
+      final ProgressDialog  pd=new ProgressDialog(mContex);
+        pd.setMessage("Loading..");
+        pd.setCancelable(true);
+
+        if (pref.getLanguage().equals("hi")) {
+            final Handler textViewHandler1 = new Handler();
+            new AsyncTask<Void, Void, Void>() {
+                @Override
+                protected Void doInBackground(Void... params) {
+                    TranslateOptions options = TranslateOptions.newBuilder()
+                            .setApiKey("AIzaSyCEQyxLkrIoD2-k_185t2EUKEc8IlggaMs")
+                            .build();
+                    Translate translate = options.getService();
+                    final Translation translation =
+                            translate.translate(reportList.get(position).getManualCategory(),
+                                    Translate.TranslateOption.sourceLanguage("en"),  Translate.TranslateOption.targetLanguage(pref.getLanguage()));
+                    textViewHandler1.post(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            Log.d("sssh", translation.getTranslatedText());
+                            String hLoc = translation.getTranslatedText();
+                            holder.tvLeaveAttendance.setText(hLoc);
+
+
+                        }
+                    });
+                    return null;
+
+
+
+                }
+
+                @Override
+                protected void onPreExecute() {
+                    super.onPreExecute();
+                    pd.show();
+
+                }
+
+                @Override
+                protected void onPostExecute(Void aVoid) {
+                    super.onPostExecute(aVoid);
+                    pd.dismiss();
+
+                }
+
+
+            }.execute();
+        }else {
+            holder.tvLeaveAttendance.setText(reportList.get(position).getManualCategory());
+        }
+
+
+        if (pref.getLanguage().equals("hi")) {
+            final Handler textViewHandler2 = new Handler();
+            new AsyncTask<Void, Void, Void>() {
+                @Override
+                protected Void doInBackground(Void... params) {
+                    TranslateOptions options = TranslateOptions.newBuilder()
+                            .setApiKey("AIzaSyCEQyxLkrIoD2-k_185t2EUKEc8IlggaMs")
+                            .build();
+                    Translate translate = options.getService();
+                    final Translation translation =
+                            translate.translate(reportList.get(position).getManualSubCategory(),
+                                    Translate.TranslateOption.sourceLanguage("en"), Translate.TranslateOption.targetLanguage(pref.getLanguage()));
+                    textViewHandler2.post(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            Log.d("sssh", translation.getTranslatedText());
+                            String hLoc = translation.getTranslatedText();
+                            holder.tvLeaveApplication.setText(hLoc);
+
+
+                        }
+                    });
+                    return null;
+
+
+                }
+
+                @Override
+                protected void onPreExecute() {
+                    super.onPreExecute();
+                    pd.show();
+
+                }
+
+                @Override
+                protected void onPostExecute(Void aVoid) {
+                    super.onPostExecute(aVoid);
+                    pd.dismiss();
+
+                }
+
+
+            }.execute();
+
+        } else
+            {
+                holder.tvLeaveApplication.setText("( " + reportList.get(position).getManualSubCategory() + " )");
+            }
+
+
+        if (pref.getLanguage().equals("hi")) {
+            final Handler textViewHandler3 = new Handler();
+            new AsyncTask<Void, Void, Void>() {
+                @Override
+                protected Void doInBackground(Void... params) {
+                    TranslateOptions options = TranslateOptions.newBuilder()
+                            .setApiKey("AIzaSyCEQyxLkrIoD2-k_185t2EUKEc8IlggaMs")
+                            .build();
+                    Translate translate = options.getService();
+                    final Translation translation =
+                            translate.translate(reportList.get(position).getManualDescription(),
+                                    Translate.TranslateOption.sourceLanguage("en"), Translate.TranslateOption.targetLanguage(pref.getLanguage()));
+                    textViewHandler3.post(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            Log.d("sssh", translation.getTranslatedText());
+                            String hLoc = translation.getTranslatedText();
+                            holder.tvDescriprtion.setText(hLoc);
+
+
+                        }
+                    });
+                    return null;
+
+
+                }
+
+                @Override
+                protected void onPreExecute() {
+                    super.onPreExecute();
+                    pd.show();
+
+                }
+
+                @Override
+                protected void onPostExecute(Void aVoid) {
+                    super.onPostExecute(aVoid);
+                    pd.dismiss();
+
+                }
+
+
+            }.execute();
+
+        } else {
+
+            holder.tvDescriprtion.setText(reportList.get(position).getManualDescription());
+        }
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,6 +203,20 @@ public class DocumentDetailsAdapter extends RecyclerView.Adapter<DocumentDetails
                 mContex.startActivity(browserIntent);
             }
         });
+
+
+        if (pref.getLanguage().equals("hi")) {
+            holder.tvDesp.setText("विवरण");
+            holder.tvButton.setText("दस्तावेज़ देखें");
+        }
+        else
+        {
+            holder.tvDesp.setText("Description");
+            holder.tvButton.setText("VIEW DOCUMENTS");
+        }
+
+
+
     }
 
 
@@ -54,12 +230,16 @@ public class DocumentDetailsAdapter extends RecyclerView.Adapter<DocumentDetails
         TextView tvLeaveAttendance;
         TextView tvLeaveApplication;
         TextView tvDescriprtion;
+        TextView tvDesp;
+        TextView tvButton;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             tvLeaveAttendance = (TextView) itemView.findViewById(R.id.tvLeaveAttendance);
             tvLeaveApplication = (TextView) itemView.findViewById(R.id.tvLeaveApplication);
             tvDescriprtion = (TextView) itemView.findViewById(R.id.tvDescriprtion);
+            tvDesp = (TextView) itemView.findViewById(R.id.tvDesp);
+            tvButton = (TextView) itemView.findViewById(R.id.tvButton);
         }
     }
 
