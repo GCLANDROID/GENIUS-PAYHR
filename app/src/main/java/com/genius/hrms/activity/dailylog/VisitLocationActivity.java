13037a47a2a24971490abf27f5475e21e41f9a18
GENIUS\sysdev_swagatam <spanda@geniusconsultant.com>
2023-08-23 18:33:40 +0530

    swagatam commit

--- app/src/main/java/com/genius/hrms/activity/dailylog/VisitLocationActivity.java
+++ app/src/main/java/com/genius/hrms/activity/dailylog/VisitLocationActivity.java
@@ -2,9 +2,7 @@ package com.genius.hrms.activity.dailylog;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-
 import android.os.Bundle;
-
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -24,14 +22,12 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.genius.hrms.R;
-import com.genius.hrms.activity.activity.EmployeeDashBoardActivity;
 import com.genius.hrms.activity.activity.UserDashBoardActivity;
 import com.genius.hrms.activity.adapter.VisitingLocationAdapter;
-import com.genius.hrms.activity.attendance.AttendanceActivity;
+import com.genius.hrms.activity.dailylog.archisman.DailyLogAttendaneArchisman;
 import com.genius.hrms.activity.geofence.GeoFenceDailyLogManageActivity;
 import com.genius.hrms.activity.model.VisitingLocationModel;
 import com.genius.hrms.activity.utility.Pref;
-import com.google.android.gms.maps.model.LatLng;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.json.JSONArray;
@@ -45,21 +41,21 @@ import java.util.Date;
 
 public class VisitLocationActivity extends AppCompatActivity {
     RecyclerView rvItem;
-    ArrayList<VisitingLocationModel>itemList=new ArrayList<>();
+    ArrayList<VisitingLocationModel> itemList = new ArrayList<>();
     Button btnAdd;
     ImageView imgAdd;
-    LinearLayout llLoader,llMain,llNoData;
-    ProgressDialog pd,pd1;
+    LinearLayout llLoader, llMain, llNoData;
+    ProgressDialog pd, pd1;
     Pref pref;
     String formattedDate;
     TextView tvDate;
     FloatingActionButton fbAdd;
-    ImageView imgBack,imgHome;
+    ImageView imgBack, imgHome;
     TextView tvToolBar;
     String surl;
     String attCode;
     String frstPunch;
-    double SLongitude,SLatitude,s;
+    double SLongitude, SLatitude, s;
     String punchFrom;
 
 
@@ -72,48 +68,48 @@ public class VisitLocationActivity extends AppCompatActivity {
         onClick();
     }
 
-    private void initView(){
-        pref=new Pref(getApplicationContext());
-        rvItem=(RecyclerView)findViewById(R.id.rvItem);
+    private void initView() {
+        pref = new Pref(getApplicationContext());
+        rvItem = (RecyclerView) findViewById(R.id.rvItem);
         LinearLayoutManager layoutManager
                 = new LinearLayoutManager(VisitLocationActivity.this, LinearLayoutManager.VERTICAL, false);
         rvItem.setLayoutManager(layoutManager);
 
-        btnAdd=(Button)findViewById(R.id.btnAdd);
-        llMain=(LinearLayout)findViewById(R.id.llMain);
-        llLoader=(LinearLayout)findViewById(R.id.llLoader);
-        llNoData=(LinearLayout)findViewById(R.id.llNoData);
-        pd=new ProgressDialog(this);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        llMain = (LinearLayout) findViewById(R.id.llMain);
+        llLoader = (LinearLayout) findViewById(R.id.llLoader);
+        llNoData = (LinearLayout) findViewById(R.id.llNoData);
+        pd = new ProgressDialog(this);
         Date c = Calendar.getInstance().getTime();
         System.out.println("Current time => " + c);
 
         SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
-         formattedDate = df.format(c);
-        Log.d("formattedDate",formattedDate);
-        tvDate=(TextView)findViewById(R.id.tvDate);
+        formattedDate = df.format(c);
+        Log.d("formattedDate", formattedDate);
+        tvDate = (TextView) findViewById(R.id.tvDate);
         tvDate.setText(formattedDate);
-        pd1=new ProgressDialog(this);
-        fbAdd=(FloatingActionButton)findViewById(R.id.fbAdd);
-        imgBack=(ImageView)findViewById(R.id.imgBack);
-        imgHome=(ImageView)findViewById(R.id.imgHome);
-        tvToolBar=(TextView)findViewById(R.id.tvToolBar);
-        if (pref.getLanguage().equals("hi")){
+        pd1 = new ProgressDialog(this);
+        fbAdd = (FloatingActionButton) findViewById(R.id.fbAdd);
+        imgBack = (ImageView) findViewById(R.id.imgBack);
+        imgHome = (ImageView) findViewById(R.id.imgHome);
+        tvToolBar = (TextView) findViewById(R.id.tvToolBar);
+        if (pref.getLanguage().equals("hi")) {
             tvToolBar.setText("स्थान का दौरा किया");
             btnAdd.setText("अपनी गतिविधि शुरू करें");
-        }else {
+        } else {
             tvToolBar.setText("Visited place");
             btnAdd.setText("Start your activity");
         }
     }
 
-    private void getItem(){
+    private void getItem() {
         llLoader.setVisibility(View.VISIBLE);
         llMain.setVisibility(View.GONE);
         llNoData.setVisibility(View.GONE);
-        if (pref.getSecurityCode().equals("123")){
-           surl= "http://111.93.182.174/GeniusiOSApi/api//get_OfflineDailyLogActivity?AEMEmployeeID=" + pref.getEmpId() + "&Year=0&Month=0&SecurityCode=" + pref.getSecurityCode()+"&AttendanceDate="+formattedDate+"&Operation=1";
-        }else {
-            surl = pref.getIpAddress()+"GHRMSApi/api/get_OfflineDailyLogActivity?AEMEmployeeID=" + pref.getEmpId() + "&Year=0&Month=0&SecurityCode=" + pref.getSecurityCode()+"&AttendanceDate="+formattedDate+"&Operation=1";
+        if (pref.getSecurityCode().equals("123")) {
+            surl = "http://111.93.182.174/GeniusiOSApi/api//get_OfflineDailyLogActivity?AEMEmployeeID=" + pref.getEmpId() + "&Year=0&Month=0&SecurityCode=" + pref.getSecurityCode() + "&AttendanceDate=" + formattedDate + "&Operation=1";
+        } else {
+            surl = pref.getIpAddress() + "GHRMSApi/api/get_OfflineDailyLogActivity?AEMEmployeeID=" + pref.getEmpId() + "&Year=0&Month=0&SecurityCode=" + pref.getSecurityCode() + "&AttendanceDate=" + formattedDate + "&Operation=1";
 
         }
         Log.d("inputactivity", surl);
@@ -136,24 +132,24 @@ public class VisitLocationActivity extends AppCompatActivity {
                             if (responseStatus) {
                                 // Toast.makeText(getApplicationContext(),responseText,Toast.LENGTH_LONG).show();
                                 JSONArray responseData = job1.optJSONArray("responseData");
-                                for (int i = 0; i <responseData.length(); i++) {
+                                for (int i = 0; i < responseData.length(); i++) {
                                     JSONObject obj = responseData.getJSONObject(i);
 
                                     String PunchInTime = obj.optString("PunchInTime");
                                     String AddressIN = obj.optString("AddressIN");
-                                    String LongitudeIN=obj.optString("LongitudeIN");
-                                    String LatitudeIN=obj.optString("LatitudeIN");
+                                    String LongitudeIN = obj.optString("LongitudeIN");
+                                    String LatitudeIN = obj.optString("LatitudeIN");
 
-                                    VisitingLocationModel obj2 = new VisitingLocationModel(AddressIN,PunchInTime,LatitudeIN,LongitudeIN);
+                                    VisitingLocationModel obj2 = new VisitingLocationModel(AddressIN, PunchInTime, LatitudeIN, LongitudeIN);
                                     itemList.add(obj2);
 
 
                                 }
 
 
-                                JSONObject frstPunchObj=responseData.optJSONObject(0);
-                                frstPunch=frstPunchObj.optString("PunchInTime");
-                                punchFrom=frstPunchObj.optString("PunchFrom");
+                                JSONObject frstPunchObj = responseData.optJSONObject(0);
+                                frstPunch = frstPunchObj.optString("PunchInTime");
+                                punchFrom = frstPunchObj.optString("PunchFrom");
                                 setAdapter();
                                 llLoader.setVisibility(View.GONE);
                                 llMain.setVisibility(View.VISIBLE);
@@ -164,7 +160,7 @@ public class VisitLocationActivity extends AppCompatActivity {
                                 llLoader.setVisibility(View.GONE);
                                 llMain.setVisibility(View.GONE);
                                 llNoData.setVisibility(View.VISIBLE);
-                                frstPunch="0";
+                                frstPunch = "0";
 
                             }
 
@@ -193,34 +189,39 @@ public class VisitLocationActivity extends AppCompatActivity {
         RequestQueue requestQueue = Volley.newRequestQueue(VisitLocationActivity.this);
         requestQueue.add(stringRequest);
     }
-    private void setAdapter(){
-        VisitingLocationAdapter vAdapter=new VisitingLocationAdapter(itemList,getApplicationContext());
+
+    private void setAdapter() {
+        VisitingLocationAdapter vAdapter = new VisitingLocationAdapter(itemList, getApplicationContext());
         rvItem.setAdapter(vAdapter);
     }
-    private void onClick(){
+
+    private void onClick() {
         fbAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                    if (pref.getSecurityCode().equals("1153")){
+                if (pref.getSecurityCode().equals("1153")) {
 
-                       getAttendanceInformationForSmart();
+                    getAttendanceInformationForSmart();
                         /*Intent intent = new Intent(VisitLocationActivity.this, OfflineDailyLogManageActivity.class);
                         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
                         startActivity(intent);*/
-                    }else if (pref.getSecurityCode().equals("1157")){
+                } else if (pref.getSecurityCode().equals("1157")) {
 
-                        Intent intent = new Intent(VisitLocationActivity.this, DailyLogMarkActivity.class);
-                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-                        startActivity(intent);
-                    }
-                    else {
-                        Intent intent = new Intent(VisitLocationActivity.this, DailyLogMarkActivity.class);
-                        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-                        startActivity(intent);
-                    }
-
+                    Intent intent = new Intent(VisitLocationActivity.this, DailyLogMarkActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                } else if (pref.getSecurityCode().equals("1000")) {
+                    Intent intent = new Intent(VisitLocationActivity.this, DailyLogAttendaneArchisman.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                } else {
+                    Intent intent = new Intent(VisitLocationActivity.this, DailyLogMarkActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
                 }
+
+            }
 
         });
 
@@ -228,19 +229,22 @@ public class VisitLocationActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                if (pref.getSecurityCode().equals("1153")){
+                if (pref.getSecurityCode().equals("1153")) {
 
                     getAttendanceInformationForSmart();
                    /* Intent intent = new Intent(VisitLocationActivity.this, OfflineDailyLogManageActivity.class);
                     intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
                     startActivity(intent);*/
-                }else if (pref.getSecurityCode().equals("1157")){
+                } else if (pref.getSecurityCode().equals("1157")) {
 
                     Intent intent = new Intent(VisitLocationActivity.this, DailyLogMarkActivity.class);
                     intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
                     startActivity(intent);
-                }
-                else {
+                } else if (pref.getSecurityCode().equals("1000")) {
+                    Intent intent = new Intent(VisitLocationActivity.this, DailyLogAttendaneArchisman.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                } else {
                     Intent intent = new Intent(VisitLocationActivity.this, DailyLogMarkActivity.class);
                     intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
                     startActivity(intent);
@@ -258,7 +262,7 @@ public class VisitLocationActivity extends AppCompatActivity {
         imgHome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(VisitLocationActivity.this, UserDashBoardActivity.class);
+                Intent intent = new Intent(VisitLocationActivity.this, UserDashBoardActivity.class);
                 startActivity(intent);
                 finish();
             }
@@ -311,8 +315,8 @@ public class VisitLocationActivity extends AppCompatActivity {
                             Intent intent = new Intent(VisitLocationActivity.this, SmartJuleDailyLogActivity.class);
                             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
                             intent.putExtra("attCode", attCode);
-                            intent.putExtra("frstPunch",frstPunch);
-                            intent.putExtra("punchFrom",punchFrom);
+                            intent.putExtra("frstPunch", frstPunch);
+                            intent.putExtra("punchFrom", punchFrom);
                             startActivity(intent);
 
 
@@ -339,7 +343,7 @@ public class VisitLocationActivity extends AppCompatActivity {
 
     private void getValueForGeoFenceForIntas() {
 
-        String surl =  pref.getIpAddress()+"GHRMSApi/api/get_EmployeeGeofenceConfigure?EmployeeId=" + pref.getEmpId() + "&GeoFenceId=0&Operation=5&SecurityCode=" + pref.getSecurityCode();
+        String surl = pref.getIpAddress() + "GHRMSApi/api/get_EmployeeGeofenceConfigure?EmployeeId=" + pref.getEmpId() + "&GeoFenceId=0&Operation=5&SecurityCode=" + pref.getSecurityCode();
         Log.d("valuefetechurl", surl);
         final ProgressDialog pd = new ProgressDialog(this);
         pd.setMessage("Loading..");
@@ -360,23 +364,22 @@ public class VisitLocationActivity extends AppCompatActivity {
                             if (responseStatus) {
 
                                 JSONArray responseData = job1.optJSONArray("responseData");
-                                String jsonData=responseData.toString();
+                                String jsonData = responseData.toString();
                                 for (int i = 0; i < responseData.length(); i++) {
                                     JSONObject obj = responseData.getJSONObject(i);
-                                     SLatitude = Double.parseDouble(obj.optString("Latitude"));
-                                     SLongitude = Double.parseDouble(obj.optString("Longitude"));
-                                     double EndPoint = Double.parseDouble(obj.optString("Radius"));
-                                     s=EndPoint/10;
-
+                                    SLatitude = Double.parseDouble(obj.optString("Latitude"));
+                                    SLongitude = Double.parseDouble(obj.optString("Longitude"));
+                                    double EndPoint = Double.parseDouble(obj.optString("Radius"));
+                                    s = EndPoint / 10;
 
 
                                 }
 
                                 pd.dismiss();
 
-                                Intent intent=new Intent(VisitLocationActivity.this,GeoFenceDailyLogManageActivity.class);
-                                intent.putExtra("radius",s);
-                                intent.putExtra("jsonData",jsonData);
+                                Intent intent = new Intent(VisitLocationActivity.this, GeoFenceDailyLogManageActivity.class);
+                                intent.putExtra("radius", s);
+                                intent.putExtra("jsonData", jsonData);
                                 startActivity(intent);
 
 
@@ -387,8 +390,6 @@ public class VisitLocationActivity extends AppCompatActivity {
 
 
                             }
-
-
 
 
                             // boolean _status = job1.getBoolean("status");
