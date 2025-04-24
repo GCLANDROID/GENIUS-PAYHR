9c72652a14828c6912c7adbd164d7963e2c93e68
Prabir Kumar kundu <prabir_sys@geniusconsultant.com>
2025-04-24 17:48:06 +0530

    commit by Prabir 24-04-2025

--- app/src/main/java/com/genius/payhrms/activity/utility/SecurityCode.java
+++ app/src/main/java/com/genius/payhrms/activity/utility/SecurityCode.java
@@ -6,4 +6,12 @@ public class SecurityCode {
     public static String Shalimar ="1186";
     public static String Future_Foundation ="6716";
     public static String Arun_Nursery ="6715";
+    public static String Dayco ="1169";
+    public static String Matthews ="1167";
+    public static String Western_Enterprises ="1156";
+    public static String Vikramshila ="1152";
+    public static String Laxmi_Stores ="1150";
+    public static String FilFly_Center ="1149";
+    public static String Battery_Center ="1148";
+    public static String Laxmi_Motors ="1147";
 }
